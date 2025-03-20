36f1befc5665a3ad346683f56ba9c1578b544562
Yasser Fathallah <yasserfat7alah@gmail.com>
2025-03-20 16:52:24 +0200

    set up f(updateNote)

--- controller/noteController.js
+++ controller/noteController.js
@@ -62,7 +62,49 @@ exports.getNote = (req, res) => {
 
 // Update a note // still in progress
 exports.updateNote = (req, res) => {
-  res.send("UPDATE NOTE");
+   //variables intialization (required: id)
+   var id = req.body.id; // Get the id from the request
+   if(!id) { // Check if the id is empty
+
+     // Send the server response
+     res.status(400).send("ERROR: ID is required!");
+
+     // Log the error
+     console.log('================');
+     console.log('ERROR: ID is required!');
+     return;
+   }
+   
+var existingNote = memStorage.store.getItem(id); // Get the existing note from the memory storage
+   if (!existingNote) {// Check if the note is not found
+     res.status(404).send("ERROR: Note not found!");
+     console.log('================');
+     console.log('ERROR: Note not found!');
+     console.log('id: ', id);
+     return;
+   }
+console.log('Note : ', existingNote);
+
+
+var title = req.body.title; // Get the title from the request
+  if(title) { // Check if the title is not empty
+     existingNote.title = title; // Update the title
+}
+     
+var content = req.body.content; // Get the content from the request
+if(content) { // Check if the content is not empty
+     existingNote.content = content; // Update the content
+}
+
+existingNote.dateOfLastUpdate = Date.now().toLocaleString(); // log time update
+
+
+//overwriting the note
+memStorage.store.setItem(id, existingNote); // Update the note in the memory storage
+
+//server response
+res.status(201).send("SUCCESS: Note Updated!");
+console.log('Note Updated: ', existingNote);
 
 };
 
